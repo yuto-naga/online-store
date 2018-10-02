2254676207c6f8203385fa603e39eacbbf6c689c
長澤優斗 <yuto.nagasawa@soliton.co.jp>
2018-10-02 10:34:27 +0900

    generated online store entity model

---
+++ src/main/webapp/app/entities/product-category/product-category-popup.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { HttpResponse } from '@angular/common/http';
+import { ProductCategory } from './product-category.model';
+import { ProductCategoryService } from './product-category.service';
+
+@Injectable()
+export class ProductCategoryPopupService {
+    private ngbModalRef: NgbModalRef;
+
+    constructor(
+        private modalService: NgbModal,
+        private router: Router,
+        private productCategoryService: ProductCategoryService
+
+    ) {
+        this.ngbModalRef = null;
+    }
+
+    open(component: Component, id?: number | any): Promise<NgbModalRef> {
+        return new Promise<NgbModalRef>((resolve, reject) => {
+            const isOpen = this.ngbModalRef !== null;
+            if (isOpen) {
+                resolve(this.ngbModalRef);
+            }
+
+            if (id) {
+                this.productCategoryService.find(id)
+                    .subscribe((productCategoryResponse: HttpResponse<ProductCategory>) => {
+                        const productCategory: ProductCategory = productCategoryResponse.body;
+                        this.ngbModalRef = this.productCategoryModalRef(component, productCategory);
+                        resolve(this.ngbModalRef);
+                    });
+            } else {
+                // setTimeout used as a workaround for getting ExpressionChangedAfterItHasBeenCheckedError
+                setTimeout(() => {
+                    this.ngbModalRef = this.productCategoryModalRef(component, new ProductCategory());
+                    resolve(this.ngbModalRef);
+                }, 0);
+            }
+        });
+    }
+
+    productCategoryModalRef(component: Component, productCategory: ProductCategory): NgbModalRef {
+        const modalRef = this.modalService.open(component, { size: 'lg', backdrop: 'static'});
+        modalRef.componentInstance.productCategory = productCategory;
+        modalRef.result.then((result) => {
+            this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true, queryParamsHandling: 'merge' });
+            this.ngbModalRef = null;
+        }, (reason) => {
+            this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true, queryParamsHandling: 'merge' });
+            this.ngbModalRef = null;
+        });
+        return modalRef;
+    }
+}
