2063229c0ff5760fbd475386e4d85e10b35f3fa7
長澤優斗 <yuto.nagasawa@soliton.co.jp>
2018-11-29 10:56:59 +0900

    Generated with JHipster 4.14.5

---
+++ src/main/webapp/app/entities/product-category/product-category-detail.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs/Subscription';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { ProductCategory } from './product-category.model';
+import { ProductCategoryService } from './product-category.service';
+
+@Component({
+    selector: 'jhi-product-category-detail',
+    templateUrl: './product-category-detail.component.html'
+})
+export class ProductCategoryDetailComponent implements OnInit, OnDestroy {
+
+    productCategory: ProductCategory;
+    private subscription: Subscription;
+    private eventSubscriber: Subscription;
+
+    constructor(
+        private eventManager: JhiEventManager,
+        private productCategoryService: ProductCategoryService,
+        private route: ActivatedRoute
+    ) {
+    }
+
+    ngOnInit() {
+        this.subscription = this.route.params.subscribe((params) => {
+            this.load(params['id']);
+        });
+        this.registerChangeInProductCategories();
+    }
+
+    load(id) {
+        this.productCategoryService.find(id)
+            .subscribe((productCategoryResponse: HttpResponse<ProductCategory>) => {
+                this.productCategory = productCategoryResponse.body;
+            });
+    }
+    previousState() {
+        window.history.back();
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    registerChangeInProductCategories() {
+        this.eventSubscriber = this.eventManager.subscribe(
+            'productCategoryListModification',
+            (response) => this.load(this.productCategory.id)
+        );
+    }
+}
