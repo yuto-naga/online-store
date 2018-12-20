3f58136e921d039f9ba7b256578ab6be4151143f
長澤優斗 <yuto.nagasawa@soliton.co.jp>
2018-12-20 11:12:17 +0900

    add import statement

--- src/main/java/com/mycompany/store/repository/ProductOrderRepository.java
+++ src/main/java/com/mycompany/store/repository/ProductOrderRepository.java
@@ -1,18 +1,20 @@
 package com.mycompany.store.repository;
 
-import com.mycompany.store.domain.ProductOrder;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.mycompany.store.domain.ProductOrder;
 
 /**
- * Spring Data  repository for the ProductOrder entity.
+ * Spring Data repository for the ProductOrder entity.
  */
 @SuppressWarnings("unused")
 @Repository
 public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
 
-    Page<ProductOrder> findAllByCustomerUserLogin(String login, Pageable pageable);
+	Page<ProductOrder> findAllByCustomerUserLogin(String login, Pageable pageable);
 
-    ProductOrder findOneByIdAndCustomerUserLogin(Long id, String login);
+	ProductOrder findOneByIdAndCustomerUserLogin(Long id, String login);
 }
--- src/main/java/com/mycompany/store/repository/ShipmentRepository.java
+++ src/main/java/com/mycompany/store/repository/ShipmentRepository.java
@@ -1,18 +1,20 @@
 package com.mycompany.store.repository;
 
-import com.mycompany.store.domain.Shipment;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.mycompany.store.domain.Shipment;
 
 /**
- * Spring Data  repository for the Shipment entity.
+ * Spring Data repository for the Shipment entity.
  */
 @SuppressWarnings("unused")
 @Repository
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
 
-    Page<Shipment> findAllByInvoiceOrderCustomerUserLogin(String login, Pageable pageable);
+	Page<Shipment> findAllByInvoiceOrderCustomerUserLogin(String login, Pageable pageable);
 
-    Shipment findOneByIdAndInvoiceOrderCustomerUserLogin(Long id, String login);
+	Shipment findOneByIdAndInvoiceOrderCustomerUserLogin(Long id, String login);
 }
